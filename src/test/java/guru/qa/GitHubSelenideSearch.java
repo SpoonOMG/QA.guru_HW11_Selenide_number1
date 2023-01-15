66fd649cb0a0363323d886f18d33fa1cf60b3167
Михайлюк Андрей <andrey.mikhaylyuk@gazprombank.ru>
2023-01-15 19:19:39 +0300

    added tests for Enterprise and Drag and drop

--- src/test/java/guru/qa/GitHubSelenideSearch.java
+++ src/test/java/guru/qa/GitHubSelenideSearch.java
@@ -3,25 +3,29 @@ package guru.qa;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.conditions.Text;
 import com.codeborne.selenide.logevents.SelenideLogger;
 
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 
+import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class GitHubSelenideSearch {
+    private static String baseUrl = "https://github.com/";
+
     @BeforeAll
     static void testConfiguration() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Selenide.clearBrowserCookies();
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
-        Configuration.baseUrl = "https://github.com/";
+        Configuration.baseUrl = baseUrl;
     }
 
     @Test
@@ -41,4 +45,25 @@ public class GitHubSelenideSearch {
             $(".markdown-body").shouldHave(Condition.text("Using JUnit5"));
         });
     }
+
+    @Test
+    void gitHubSelenideSolutions() {
+        step("Открыть главную страницу", () -> {
+            open(baseUrl);
+        });
+        step("Навести на вкладку Solutions", () -> {
+            $(byText("Solutions")).hover();
+        });
+        step("Перейти в Enterprise, убедившись в успешном переходе на страницу", () -> {
+            $(byText("Enterprise")).click();
+            $(byText("Build like the best")).shouldBe(Condition.visible);
+        });
+    }
+
+    @Test
+    void dragNDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+    }
+
 }
